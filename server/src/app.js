231bf5e8dbaf4ba07afba754a3751f266be1b8f5
Payal  Abhale <abhalepayal491@gmail.com>
2026-01-31 13:53:59 +0530

    add

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,11 @@ const server = http.createServer(app);
 
 
 // middleware
-app.use(cors()) ;
+app.use(cors({
+    origin: "https://code-sync-beta-five.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })) ;
 app.use(express.json()); 
 
 // app.use(express.json({limit: "10kb"}))
@@ -27,7 +31,7 @@ app.use(express.json());
 // Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin:  "https://code-sync-beta-five.vercel.app",
     methods: ["GET", "POST"],
   },
 });
